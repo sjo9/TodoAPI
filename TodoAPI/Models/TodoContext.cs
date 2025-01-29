f4ea78b2ecd7ba36467c2a320e05dbd1066a4d27
Seyeon Jo <sjo9@my.centennialcollege.ca>
2025-01-29 12:19:50 -0500

    Add quiz#2 to TodoItems (Fixes #1)

--- TodoAPI/Models/TodoContext.cs
+++ TodoAPI/Models/TodoContext.cs
@@ -10,5 +10,19 @@ namespace TodoAPI.Models
         }
 
         public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+        //Q2 #6 update the code
+        public void AddQuiz2()
+        {
+            var newTodo = new TodoItem
+            {
+                Id = 3,  
+                Name = "quiz#2",  
+                IsComplete = false 
+            };
+
+            TodoItems.Add(newTodo);  
+            SaveChanges();  
+        }
     }
 }
